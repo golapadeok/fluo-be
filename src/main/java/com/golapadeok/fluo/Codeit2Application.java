6b5860a41a3d843a13c02452e801b9daea0a2b20
didado <idejrdud@gmail.com>
2024-03-06 14:30:48 +0900

    first commit

---
+++ src/main/java/com/golapadeok/fluo/Codeit2Application.java
@@ -0,0 +1,13 @@
+package com.golapadeok.fluo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Codeit2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Codeit2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/golapadeok/fluo/Codeit2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.golapadeok.fluo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Codeit2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
