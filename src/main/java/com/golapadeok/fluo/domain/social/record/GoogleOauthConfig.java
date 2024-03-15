718ead27de08c728cd54630d9a47c16abdadba6a
Seonoo <tjsdn22@naver.com>
2024-03-16 03:02:31 +0900

    style : yml 파일 수정 및 동작 테스트

--- src/main/java/com/golapadeok/fluo/domain/social/record/GoogleOauthConfig.java
+++ src/main/java/com/golapadeok/fluo/domain/social/record/GoogleOauthConfig.java
@@ -2,7 +2,7 @@ package com.golapadeok.fluo.domain.social.record;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-@ConfigurationProperties(prefix = "spring.oauth2.google")
+@ConfigurationProperties(prefix = "oauth2.google")
 public record GoogleOauthConfig(
     String clientId,
     String clientSecret,
