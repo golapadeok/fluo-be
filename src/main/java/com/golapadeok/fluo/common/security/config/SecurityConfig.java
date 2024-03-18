bee6ab7d24ab070d783bb546c4838867de2867f0
didado <idejrdud@gmail.com>
2024-03-18 22:58:50 +0900

    fix: h2 오류 테스트

--- src/main/java/com/golapadeok/fluo/common/security/config/SecurityConfig.java
+++ src/main/java/com/golapadeok/fluo/common/security/config/SecurityConfig.java
@@ -10,9 +10,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.parameters.P;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.AuthenticationEntryPoint;
@@ -23,6 +26,7 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Configuration
@@ -40,15 +44,37 @@ public class SecurityConfig {
     }
 
     @Bean
+    @Profile("default")
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests((authorization) -> authorization
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorization -> authorization
                         .requestMatchers(PathRequest.toH2Console()).permitAll()
                         .requestMatchers("/api/v1/auth/logout").authenticated()
-                .anyRequest().permitAll());
+                        .anyRequest().permitAll());
 
         http.sessionManagement(sessionManagement -> sessionManagement
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable);
+
+        // UsernamePasswordAuthenticationFilter 이전에 JwtAuthorizationFilter를 실행하겠다는 뜻
+        http.addFilterBefore(new JwtAuthorizationFilter(this.provider, this.memberRepository, this.blackListRepository), UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(new JwtExceptionFilter(), JwtAuthorizationFilter.class);
+
+        return http.build();
+    }
+
+    @Bean
+    @Profile({"dev", "prod"})
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests((authorization) -> authorization
+//                        .requestMatchers(PathRequest.toH2Console()).permitAll()
+                        .requestMatchers("/api/v1/auth/logout").authenticated()
+                        .anyRequest().permitAll());
+
+        http.sessionManagement(sessionManagement -> sessionManagement
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .formLogin(formLogin -> formLogin.disable())
                 .httpBasic(httpBasic -> httpBasic.disable());
 
@@ -64,7 +90,7 @@ public class SecurityConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(Arrays.asList("*"));
+        config.setAllowedOrigins(List.of("*"));
         config.setAllowCredentials(true);
         config.setAllowedMethods(Collections.singletonList("*"));
         config.setAllowedHeaders(Collections.singletonList("*"));
