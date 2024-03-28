c9b93d872c41158829ebe0bcc7335877041537f6
seonoo <tjsdn22@naver.com>
2024-03-28 14:16:32 +0900

    feat : #37 관리자 역할일 경우 수정 / 삭제 안되도록 기능 추가

--- src/main/java/com/golapadeok/fluo/domain/role/exception/RoleErrorStatus.java
+++ src/main/java/com/golapadeok/fluo/domain/role/exception/RoleErrorStatus.java
@@ -8,7 +8,9 @@ public enum RoleErrorStatus {
     NOT_FOUND_WORKSPACE(400, "존재하지 않는 워크스페이스 입니다."),
     NOT_FOUND_ROLE(400, "존재하지 않는 역할 입니다."),
     DUPLICATION_NAME(400, "중복된 역할 이름 있습니다."),
-    LIMIT_EXCEEDED_FOR_CREATION(400, "생성할 수 있는 역할 개수를 초과했습니다.");
+    LIMIT_EXCEEDED_FOR_CREATION(400, "생성할 수 있는 역할 개수를 초과했습니다."),
+    NOT_UPDATE_ADMIN_ROLE(400, "관리자 역할을 수정할 수 없습니다."),
+    NOT_DELETE_ADMIN_ROLE(400, "관리자 역할을 삭제할 수 없습니다.");
 
     private final int status;
     private final String message;
--- src/main/java/com/golapadeok/fluo/domain/role/service/RoleService.java
+++ src/main/java/com/golapadeok/fluo/domain/role/service/RoleService.java
@@ -75,6 +75,11 @@ public class RoleService {
         Role role = this.roleRepository.findById(Long.valueOf(roleId))
                 .orElseThrow(() -> new RoleException(RoleErrorStatus.NOT_FOUND_ROLE));
 
+        // 역할의 이름이 관리자이고, 모든 권한을 갖고있으면(개수) 수정 못하도록 막기
+        if(role.getName().equals("관리자") && role.getRoleList().size() == 10) {
+            throw new RoleException(RoleErrorStatus.NOT_DELETE_ADMIN_ROLE);
+        }
+
         this.roleRepository.delete(role);
 
         String message = "역할이 삭제되었습니다.";
@@ -87,6 +92,11 @@ public class RoleService {
                 .orElseThrow(() -> new RoleException(RoleErrorStatus.NOT_FOUND_ROLE));
 
         log.info("role : {}", role.toString());
+
+        // 역할의 이름이 관리자이고, 모든 권한을 갖고있으면(개수) 수정 못하도록 막기
+        if(role.getName().equals("관리자") && role.getRoleList().size() == 10) {
+            throw new RoleException(RoleErrorStatus.NOT_UPDATE_ADMIN_ROLE);
+        }
 
         // 역할 업데이트
         role.updateRole(request);
