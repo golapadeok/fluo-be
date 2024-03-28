a8a19f8813a654db6ba2dc10e60b4f83ac4ceb0a
didado <idejrdud@gmail.com>
2024-03-28 16:32:28 +0900

    feat: #4 업무 API AuthCheck 임시 주석

--- src/main/java/com/golapadeok/fluo/domain/task/api/TaskController.java
+++ src/main/java/com/golapadeok/fluo/domain/task/api/TaskController.java
@@ -38,7 +38,7 @@ public class TaskController {
         return ResponseEntity.ok(taskSearchService.search(taskId));
     }
 
-    @AuthCheck(Credential.CREATE_TASK)
+//    @AuthCheck(Credential.CREATE_TASK)
     @PostMapping
     @Operation(summary = "업무 생성 API", description = "새로운 업무를 생성합니다.")
     public ResponseEntity<TaskDetailResponse> createTask(
@@ -49,7 +49,7 @@ public class TaskController {
                 .body(taskCreateService.createTask(request));
     }
 
-    @AuthCheck(Credential.MODIFY_TASK)
+//    @AuthCheck(Credential.MODIFY_TASK)
     @PutMapping("/{taskId}")
     @Operation(summary = "업무 수정 API", description = "해당 업무를 수정합니다.")
     public ResponseEntity<TaskDetailResponse> updateTask(
@@ -58,7 +58,7 @@ public class TaskController {
         return ResponseEntity.ok(taskUpdateService.update(taskId, request));
     }
 
-    @AuthCheck(Credential.DELETE_TASK)
+//    @AuthCheck(Credential.DELETE_TASK)
     @DeleteMapping("/{taskId}")
     public ResponseEntity<TaskDeleteResponse> deleteTask(
             @PathVariable("taskId") Integer taskId) {
