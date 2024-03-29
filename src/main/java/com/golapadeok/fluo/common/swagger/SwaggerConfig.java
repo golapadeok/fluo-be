5d204450ad4de8bd36cd3d17c834f663f1716128
didado <idejrdud@gmail.com>
2024-03-29 15:50:08 +0900

    feat: swagger JWT 설정 추가

--- src/main/java/com/golapadeok/fluo/common/swagger/SwaggerConfig.java
+++ src/main/java/com/golapadeok/fluo/common/swagger/SwaggerConfig.java
@@ -1,7 +1,11 @@
 package com.golapadeok.fluo.common.swagger;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,10 +13,19 @@ import org.springframework.context.annotation.Configuration;
 public class SwaggerConfig {
 
     @Bean
-    public OpenAPI openAPI(){
+    public OpenAPI openAPI() {
+        final String securitySchemeName = "bearerAuth";
         return new OpenAPI().info(new Info()
-                .title("fluo-api")
-                .description("fluo 업무협업 api"));
+                        .title("fluo-api")
+                        .description("fluo 업무협업 api"))
+                .addSecurityItem(new SecurityRequirement()
+                        .addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName, new SecurityScheme()
+                                .name(securitySchemeName)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
     }
 
 }
