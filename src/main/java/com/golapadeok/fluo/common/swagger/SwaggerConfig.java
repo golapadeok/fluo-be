d4cb2cfcbb02d8d19a1974c4180e81492ed8104f
didado <idejrdud@gmail.com>
2024-04-01 15:05:30 +0900

    feat: swgger 주소 추가

--- src/main/java/com/golapadeok/fluo/common/swagger/SwaggerConfig.java
+++ src/main/java/com/golapadeok/fluo/common/swagger/SwaggerConfig.java
@@ -6,8 +6,11 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -15,9 +18,20 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
         final String securitySchemeName = "bearerAuth";
+
+        Server local = new Server();
+        local.setUrl("https://localhost:9999");
+
+        Server dev = new Server();
+        dev.setUrl("http://3.36.174.59:8090");
+
+        Server prod = new Server();
+        prod.setUrl("https://project-application.shop:443");
+
         return new OpenAPI().info(new Info()
                         .title("fluo-api")
                         .description("fluo 업무협업 api"))
+                .servers(List.of(local, dev, prod))
                 .addSecurityItem(new SecurityRequirement()
                         .addList(securitySchemeName))
                 .components(new Components()
