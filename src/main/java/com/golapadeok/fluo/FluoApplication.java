e63daa621c799c1e4d741956f310de532a92d2cd
seonoo <tjsdn22@naver.com>
2024-04-01 15:22:04 +0900

    feat : Swagger의 cors 해결을 위해 @OpenAPIDefinition 추가

--- src/main/java/com/golapadeok/fluo/FluoApplication.java
+++ src/main/java/com/golapadeok/fluo/FluoApplication.java
@@ -1,9 +1,16 @@
 package com.golapadeok.fluo;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+@OpenAPIDefinition(
+        servers = {
+                @Server(url="/", description = "Default Server url")
+        }
+)
 @ConfigurationPropertiesScan
 @SpringBootApplication
 public class FluoApplication {
