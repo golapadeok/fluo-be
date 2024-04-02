5d5a5a7cdec86535e6a51ea8cca3f5b3c724180f
Seonoo <tjsdn22@naver.com>
2024-04-03 00:41:04 +0900

    feat : interceptor 테스트

--- src/main/java/com/golapadeok/fluo/common/config/WebConfig.java
+++ src/main/java/com/golapadeok/fluo/common/config/WebConfig.java
@@ -1,14 +1,20 @@
 package com.golapadeok.fluo.common.config;
 
+import com.golapadeok.fluo.common.annotation.interception.AuthCheckInterceptor;
 import com.golapadeok.fluo.domain.social.converter.SocialConverter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.http.HttpHeaders;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@RequiredArgsConstructor
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    private final AuthCheckInterceptor authCheckInterceptor;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
@@ -26,5 +32,11 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addFormatters(FormatterRegistry registry) {
         registry.addConverter(new SocialConverter());
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authCheckInterceptor)
+                .addPathPatterns("*");
     }
 }
