0b4514ebd8fb28111fca6c8930a8967a963989c8
Seonoo <tjsdn22@naver.com>
2024-04-02 21:29:58 +0900

    test : 엑세스 토큰 만료시간 변경

--- src/main/java/com/golapadeok/fluo/common/jwt/JwtTokenProvider.java
+++ src/main/java/com/golapadeok/fluo/common/jwt/JwtTokenProvider.java
@@ -20,9 +20,9 @@ public class JwtTokenProvider {
     @Value("${jwt.secretKey}")
     private String secretKey;
 
-    private static final long accessTokenExpiredTime = 1000L * 60L * 60L * 24L; // 1일
+//    private static final long accessTokenExpiredTime = 1000L * 60L * 60L * 24L; // 1일
     private static final long refreshTokenExpiredTime = 1000L * 60L * 60L * 24L * 14L; // 2주
-//    private final long accessTokenExpiredTime = 1000L;
+    private final long accessTokenExpiredTime = 1000L;
 //    private final long refreshTokenExpiredTime = 1000L; // 1일
     private final String authorization = "Authorization";
     private final String tokenPrefix = "Bearer ";
