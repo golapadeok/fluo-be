3ae92bfd63fb221a2921f3d9aa24bef8a08de5ed
Seonoo <tjsdn22@naver.com>
2024-04-04 00:23:16 +0900

    feat : 워크스페이스 회원목록 조회 response 멤버 단일로 변경

---
+++ src/main/java/com/golapadeok/fluo/domain/tag/dto/request/TagUpdateRequest.java
@@ -0,0 +1,30 @@
+package com.golapadeok.fluo.domain.tag.dto.request;
+
+import com.golapadeok.fluo.domain.tag.dto.ColorCode;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class TagUpdateRequest {
+//    @NotNull(message = "워크스페이스 아이디는 필수값 입니다.")
+//    @Schema(description = "태그가 포함된 워크스페이스 아이디", example = "1")
+//    private Integer workspaceId;
+
+    @NotEmpty(message = "태그 이름은 필수값 입니다.")
+    @Schema(description = "태그 이름", example = "tagName")
+    private String name;
+
+    @NotEmpty(message = "색상 코드는 필수값 입니다.")
+    @Schema(description = "색상 코드", example = "######")
+    private String colorCode;
+
+    public ColorCode getColorCode() {
+        return ColorCode.valueOf(colorCode.toUpperCase());
+    }
+
+}
